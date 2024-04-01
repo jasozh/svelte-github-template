ee91c7c7f6837412f6dc102c8d73d3b0667ee265
Jason Zheng <jasonz4200@gmail.com>
2024-04-01 17:42:35 -0400

    Fix base path and add adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 
-const BASE_PATH = "svelte-github-template";
+const BASE_PATH = "/svelte-github-template";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
