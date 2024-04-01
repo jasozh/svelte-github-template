417567393fe5a0cf899608e0ad5fd6b8cd6df17f
Jason Zheng <jasonz4200@gmail.com>
2024-04-01 18:26:08 -0400

    Style landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="min-w-screen min-h-screen flex justify-center">
+  <div class="flex flex-col items-center mt-32">
+    <img src="/favicon.png" alt="Svelte logo" class="mb-4" />
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+      Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+  </div>
+</div>
