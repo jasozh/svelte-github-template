826051b748904a6ce51404bddcd29073e6e6fe99
Jason Zheng <jasonz4200@gmail.com>
2024-04-01 18:29:54 -0400

    Fix image path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="min-w-screen min-h-screen flex justify-center">
   <div class="flex flex-col items-center mt-32">
-    <img src="/favicon.png" alt="Svelte logo" class="mb-4" />
+    <img src="favicon.png" alt="Svelte logo" class="mb-4" />
     <h1>Welcome to SvelteKit</h1>
     <p>
       Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
