066e90ed80aab512e1dfd6add2f9251e0edf8902
Jason Zheng <jasonz4200@gmail.com>
2024-04-11 15:43:07 -0400

    Minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+</script>
+
 <div class="min-w-screen flex min-h-screen justify-center">
   <div class="mt-32 flex flex-col items-center">
-    <img src="favicon.png" alt="Svelte logo" class="mb-4" />
+    <img src="{base}/favicon.png" alt="Svelte logo" class="mb-4" />
     <h1>Welcome to SvelteKit</h1>
     <p>
       Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
